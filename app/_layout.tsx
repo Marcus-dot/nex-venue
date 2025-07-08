8ed3d2ff0a6de59a54aaaf97b9ab7b6218dc404d
enosintech <enos.nsambaj@gmail.com>
2025-07-08 16:48:25 +0200

    start auth for nex-venue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
+import "../global.css";
+
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [ fontsLoaded ] = useFonts({
+
+  })
+
+  useEffect(() => {
+    if(fontsLoaded) {
+      SplashScreen.hide();
+    }
+  }, [fontsLoaded]) 
+
+  if(!fontsLoaded) return null;
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
---
+++ components/app-loader.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppLoader = () => {
+  return (
+    <View className='flex-1 flex flex-col items-center justify-center'>
+      <Text className='text-5xl'>AppLoader</Text>
+    </View>
+  )
+}
+
+export default AppLoader;
